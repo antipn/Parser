6e579d26059d8e34cf9a78abc9fa01a3923dfd27
antipn <palmer@bk.ru>
2022-05-23 13:15:12 +0300

    init project

---
+++ src/test/java/com/parser/antipn/parser/ParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parser.antipn.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
